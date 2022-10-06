ca3731596ea269f2c5730166580d639ad320d551
Grum999 <grum999@grum.fr>
2022-10-06 09:46:03 +0200

    Improve PkTk - Update utils
    Update utils to take in account bug fix concerning deprecated getiterator()

--- bulicommander/bulicommander/pktk/modules/utils.py
+++ bulicommander/bulicommander/pktk/modules/utils.py
@@ -197,7 +197,7 @@ def loadXmlUi(fileName, parent):
 
     # Parse XML file and retrieve all object for which an icon is set
     tree = ET.parse(fileName)
-    for nodeParent in tree.getiterator():
+    for nodeParent in tree.iter():
         for nodeChild in nodeParent:
             if 'name' in nodeChild.attrib and nodeChild.attrib['name'] == 'icon':
                 nodeIconSet = nodeChild.find("iconset")
@@ -521,7 +521,10 @@ class Debug(object):
         """Print value to console, using argv for formatting"""
         if Debug.__enabled and isinstance(value, str):
             sys.stdout = sys.__stdout__
-            print('DEBUG:', value.format(*argv))
+            if len(argv) > 0:
+                print('DEBUG:', value.format(*argv))
+            else:
+                print('DEBUG:', value)
 
     @staticmethod
     def enabled():
